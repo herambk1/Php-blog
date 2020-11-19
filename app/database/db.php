91178fed007a63a2e8f33b3a12794784f2fb8266
herambk1 <herambk62@gmail.com>
2020-11-19 22:47:39 +0530

    base url

--- app/database/db.php
+++ app/database/db.php
@@ -6,17 +6,41 @@
         echo "<pre>", print_r($value, true), "</pre>";
     }
 
-    function selectAll($table)
+    function selectAll($table, $conditions =[])
     {
-        global $conn;
-        $sql = "SELECT * FROM $table";
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-        $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC); 
-        return $records;
-
+        if (empty($conditions)) 
+        {
+            global $conn;
+            $sql = "SELECT * FROM $table";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+            $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC); 
+            return $records;       
+        } 
+        else 
+        {
+            $i = 0;
+            foreach ($conditions as $key => $value) 
+            {
+                if ($i === 0) 
+                {
+                  $sql = $sql . " WHERE $key=$value";
+                } 
+                else 
+                {
+                    $sql = $sql . " AND $key=$value";
+                }
+                $i++;
+            }
+             dd($sql);
+        }
     }
 
-    $users = selectAll('users');
+    $conditions =[
+        'admin' => 1,
+        'username' => 'raisoni'
+    ];
+
+    $users = selectAll('users', $conditions);
     dd($users);
    
